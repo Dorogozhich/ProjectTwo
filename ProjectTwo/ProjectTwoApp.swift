6ee93898536952e2c78a6c667eedd87f33a185aa
dorogozhich <dorogozhich@gmail.com>
2024-02-22 11:41:44 +0200

    Initial Commit

---
+++ ProjectTwo/ProjectTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTwoApp.swift
+//  ProjectTwo
+//
+//  Created by Alexandr Vykhodets on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
